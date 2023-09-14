b89024b3ffa76b13776ad49baf410ea71e5d4331
Jonathan Bloedow <jhb4@cornell.edu>
2023-09-14 16:36:43 -0700

    Point manifest to 2018 files.

--- examples/blantyre_noHINT/from_files/manifest.py
+++ examples/blantyre_noHINT/from_files/manifest.py
@@ -1,10 +1,12 @@
 import os
 requirements = "./requirements.txt"
 #model_dl_dir = "stash"
-model_dl_dir = "model-from-to"
+#model_dl_dir = "model-from-to"
+model_dl_dir = "model_2018"
 schema_file=os.path.join(model_dl_dir, "schema.json")
 eradication_path=os.path.join(model_dl_dir, "Eradication")
 assets_input_dir="Assets"
 reporters=model_dl_dir
-sif="dtk_centos.id"
+#sif="dtk_centos.id"
+sif="dtk_centos_2018.id"
 world_bank_dataset="../world_bank_dataset.csv"
