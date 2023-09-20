6fe9c48959084b6c8e215f0e7a67e5e67e0e030f
Jonathan Bloedow <jhb4@cornell.edu>
2023-09-19 22:34:37 -0700

    Added requirements.txt for 2018 model.

---
+++ emodpy_typhoid/config.py
@@ -0,0 +1,12 @@
+def cleanup_for_2018_mode( config ):
+    # when using 2018 binary
+    config.parameters.pop( "Serialized_Population_Filenames" )
+    config.parameters.pop( "Serialization_Time_Steps" )
+    config.parameters.pop( "Demographics_Filename" )
+
+    config.parameters.Incubation_Period_Distribution = "FIXED_DURATION" # hack
+    config.parameters.Infectious_Period_Distribution = "FIXED_DURATION" # hack
+    config.parameters.Base_Incubation_Period = 1
+    config.parameters.Base_Infectious_Period = 1
+    config.parameters["Listed_Events"] = []
+
