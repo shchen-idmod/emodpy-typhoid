ba300a79169b6921ceafb377aad15c36684da5ba
Jonathan Bloedow <jhb4@cornell.edu>
2023-10-05 15:16:17 -0700

    Adding working example adding some HINT groups. Property Value Changing being weird though. Also Property Report.

---
+++ examples/HINTy/manifest.py
@@ -0,0 +1,9 @@
+import os
+requirements = "./requirements.txt"
+model_dl_dir = "stash2"
+schema_file=os.path.join(model_dl_dir, "schema.json")
+eradication_path=os.path.join(model_dl_dir, "Eradication")
+assets_input_dir="Assets"
+reporters=model_dl_dir
+sif="dtk_centos_2018.id"
+world_bank_dataset="../world_bank_dataset.csv"
