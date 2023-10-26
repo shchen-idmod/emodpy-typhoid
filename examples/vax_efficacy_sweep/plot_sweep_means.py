9b977a3a088f4b1c40b4789ea4419f61d403fb64
Jonathan Bloedow <jhb4@cornell.edu>
2023-10-25 18:42:08 -0700

    Revert default channel to CONTACT incidence.

--- examples/vax_efficacy_sweep/plot_sweep_means.py
+++ examples/vax_efficacy_sweep/plot_sweep_means.py
@@ -3,8 +3,9 @@ matplotlib.use("TkAgg")
 import emod_api.channelreports.plot_icj_means as plotter
 exp_id = "latest_experiment"
 #chan = "New Infections By Route (CONTACT)"
-#chan = "New Infections By Route (CONTACT)_CUMULATIVE"
-chan = "New Infections By Route (ENVIRONMENT)_CUMULATIVE"
-chan_data = plotter.collect(exp_id, chan, tag="efficacy=SWEEP")
+chan = "New Infections By Route (CONTACT)_CUMULATIVE"
+#chan = "New Infections By Route (ENVIRONMENT)_CUMULATIVE"
+#chan_data = plotter.collect(exp_id, chan, tag="coverage_ria=SWEEP")
+chan_data = plotter.collect(exp_id, chan, tag="decay=SWEEP")
 plotter.display(chan_data, False, chan, exp_id)
 
